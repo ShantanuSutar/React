25db8c06ded69021ae5465b986a2cba8c7543c4a
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-10-27 11:59:30 +0530

    Intro to Create React App
    1. Creating our own app (my_app_hello)
    2. Modules: Import and Export (our_own_app)

---
+++ Create React App/our_own_app/src/helpers.js
@@ -0,0 +1,17 @@
+function helpful() {
+    console.log('I DID A SUPER HELPFUL THING')
+}
+
+function sort() {
+    console.log('All Sorted!')
+}
+
+function sing() {
+    console.log('LA LA LA ')
+}
+
+
+
+export default helpful;
+
+export { sort, sing };
