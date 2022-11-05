8f3ba02942f7f75cf8cf5a90de723eba055f605b
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-11-06 00:51:07 +0530

    Intitializing state

---
+++ Introducing State/intializing state/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import Game from './Game';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Game
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
