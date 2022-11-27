6dce1619c5e2b55dff8415b8855a5bbb66b31aff
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-11-27 23:18:56 +0530

    remove function
    using setState to delete a number

---
+++ A World of React Events/Passing Methods to Child Components/number-list/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NumberList from './NumberList';
+
+function App() {
+  return (
+    <div className="App">
+      <NumberList />
+    </div>
+  );
+}
+
+export default App;
