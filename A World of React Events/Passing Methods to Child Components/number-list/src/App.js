e8c7d866cb8668e802cea871c778da1090e84fee
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-11-27 23:35:00 +0530

    Better way

--- A World of React Events/Passing Methods to Child Components/number-list/src/App.js
+++ A World of React Events/Passing Methods to Child Components/number-list/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import NumberList from './NumberList';
-
+import BetterNumberList from './BetterNumberList';
 function App() {
   return (
     <div className="App">
       <NumberList />
+      <BetterNumberList />
     </div>
   );
 }
