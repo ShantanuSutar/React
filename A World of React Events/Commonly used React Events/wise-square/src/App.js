8a196e2cac1eb8da50679cac871ad5d6675156b6
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-11-27 15:58:06 +0530

    Binding: Method 1

--- A World of React Events/Commonly used React Events/wise-square/src/App.js
+++ A World of React Events/Commonly used React Events/wise-square/src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import WiseSquare from './WiseSquare';
 import AnnoyingForm from './AnnoyingForm';
 import CopyDemo from './CopyDemo';
+import WiseSquareWithoutProps from './WiseSquareWithoutProps';
 function App() {
   return (
     <div className="App">
       <h1>React Events!!</h1>
       <WiseSquare />
+      <br></br>
+      <WiseSquareWithoutProps />
       <AnnoyingForm />
       <CopyDemo />
+
     </div>
   );
 }
