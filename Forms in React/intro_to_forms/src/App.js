3f472bc6e0ae9355ec759481ffe9a1a7bdd1f3d4
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-12-08 23:37:16 +0530

    Handling multiple Inputs

--- Forms in React/intro_to_forms/src/App.js
+++ Forms in React/intro_to_forms/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form';
+import MultipleForm from './MultipleForm';
 function App() {
   return (
     <div className="App">
       <Form />
+      <MultipleForm />
     </div>
   );
 }
