2d34a7ff0aac5d077d227eaf59dbdae2b0b3d7dd
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-12-10 18:54:36 +0530

    Removing Color Boxes

--- Forms in React/forms_exercise/src/BoxList.js
+++ Forms in React/forms_exercise/src/BoxList.js
@@ -4,9 +4,16 @@ import NewBoxForm from "./NewBoxForm";
 class BoxList extends Component {
   constructor(props) {
     super(props);
-    this.state = { boxes: [{ width: 10, height: 40, color: 'teal' }] };
+    this.state = { boxes: [] };
     this.create = this.create.bind(this);
   }
+
+  remove(id) {
+    this.setState({
+      boxes: this.state.boxes.filter(box => box.id !== id)
+    })
+  }
+
   create(newBox) {
     this.setState({
       boxes: [...this.state.boxes, newBox]
@@ -15,16 +22,20 @@ class BoxList extends Component {
   render() {
     const boxes = this.state.boxes.map(box => (
       <Box
+        key={box.id}
+        id={box.id}
         width={box.width}
         height={box.height}
         color={box.color}
+        removeBox={() => this.remove(box.id)}
       />
     ))
     return (
       <div>
         <h1>Color Box Maker Thingy</h1>
+        <NewBoxForm createBox={this.create}
+        />
         {boxes}
-        <NewBoxForm createBox={this.create} />
       </div >
     );
   }
--- Forms in React/forms_exercise/src/NewBoxForm.js
+++ Forms in React/forms_exercise/src/NewBoxForm.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-
+import { v4 as uuidv4 } from 'uuid';
 
 class NewBoxForm extends Component {
     constructor(props) {
@@ -11,7 +11,8 @@ class NewBoxForm extends Component {
 
     handleSubmit(evt) {
         evt.preventDefault();
-        this.props.createBox(this.state);
+        const newBox = { ...this.state, id: uuidv4() };
+        this.props.createBox(newBox);
         this.setState({
             height: "",
             width: "",
